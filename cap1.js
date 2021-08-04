ad03f4705f3f3211d973e6f64b363dd281639757
unknown <ldxsoria@gmail.com>
2021-08-04 11:44:02 -0500

    inicio

---
+++ cap1.js
@@ -0,0 +1,25 @@
+dinero1 = prompt('Cuanto dinero tienes?');
+dinero2 = prompt('Cuanto dinero tienes?');
+dinero3 = prompt('Cuanto dinero tienes?');
+
+if (dinero1 >= 0.6 && dinero1 <= 1) {
+    alert("Compra el helado de agua")
+}
+else if (dinero1 >= 1 && dinero1 <= 1.6) {
+    alert("Compra el helado de crema")
+}
+else if (dinero1 >= 1.6 && dinero1 <= 1.7) {
+    alert("Compra el helado marca heladix")
+}
+else if (dinero1 >= 1.7 && dinero1 <= 1.8) {
+    alert("Compra el helado marca heladovich")
+}
+else if (dinero1 >= 1.8 && dinero1 <= 2.9) {
+    alert("Compra el helado marca heladim")
+}
+else if (dinero1 >= 2.9) {
+    alert("Compra el helado con confites")
+}
+else {
+    alert("No te alcanza para un helado")
+}
