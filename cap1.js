c72611d26d4d195dc21812d5f39f10351f697941
ldxsoria <ldxsoria@gmail.com>
2021-08-04 13:07:19 -0500

    update cap1.js

--- cap1.js
+++ cap1.js
@@ -4,12 +4,15 @@ dinero3 = prompt('Cuanto dinero tienes?');
 
 if (dinero1 >= 0.6 && dinero1 <= 1) {
     alert("Compra el helado de agua")
+    alert("y tu vuelto es" - 0.6)
 }
 else if (dinero1 >= 1 && dinero1 <= 1.6) {
     alert("Compra el helado de crema")
+    alert("y tu vuelto es" - 0.6)
 }
 else if (dinero1 >= 1.6 && dinero1 <= 1.7) {
     alert("Compra el helado marca heladix")
+    
 }
 else if (dinero1 >= 1.7 && dinero1 <= 1.8) {
     alert("Compra el helado marca heladovich")
