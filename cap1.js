60817fdb68104117c151e897f8f02faab2c13ad7
ldxsoria <ldxsoria@gmail.com>
2021-08-04 22:51:11 -0500

    update cap1.js

--- cap1.js
+++ cap1.js
@@ -6,28 +6,28 @@ dinero1 = parseInt(dinero1)
 
 if (dinero1 >= 0.6 && dinero1 <= 1) {
     alert("Compra el helado de agua")
-    alert("y tu vuelto es" + dinero1 - 0.6)
+    alert("y tu vuelto es" + (dinero1 - 0.6))
 }
 else if (dinero1 >= 1 && dinero1 <= 1.6) {
     alert("Compra el helado de crema")
-    alert("y tu vuelto es" + dinero1 - 1)
+    alert("y tu vuelto es" + (dinero1 - 1))
 }
 else if (dinero1 >= 1.6 && dinero1 <= 1.7) {
     alert("Compra el helado marca heladix")
-    alert("y tu vuelto es" + dinero1 - 1.6)
+    alert("y tu vuelto es" + (dinero1 - 1.6))
     
 }
 else if (dinero1 >= 1.7 && dinero1 <= 1.8) {
     alert("Compra el helado marca heladovich")
-    alert("y tu vuelto es" + dinero1 - 1.7)
+    alert("y tu vuelto es" + (dinero1 - 1.7))
 }
 else if (dinero1 >= 1.8 && dinero1 <= 2.9) {
     alert("Compra el helado marca heladim")
-    alert("y tu vuelto es" + dinero1 - 1.8)
+    alert("y tu vuelto es" + (dinero1 - 1.8))
 }
 else if (dinero1 >= 2.9) {
     alert("Compra el helado con confites")
-    alert("y tu vuelto es" + dinero1 - 2.9)
+    alert("y tu vuelto es" + (dinero1 - 2.9))
 }
 else {
     alert("No te alcanza para un helado")
